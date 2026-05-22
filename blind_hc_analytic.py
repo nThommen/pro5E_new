529de59cadceded963af5dc821626d5b3992f511
nicolaot <nicolas.thommen1@students.fhnw.ch>
2026-05-22 13:14:54 +0200

    changed load_network funtion to empty net of loads if generation is tested for worst case scenario

--- blind_hc_analytic.py
+++ blind_hc_analytic.py
@@ -8,10 +8,16 @@ import kaleido
 from pandapower.plotting.plotly import pf_res_plotly
 
 #%% Functions
-# Load a desired network (simbench for now)
-def load_network():
+# Load a desired network and set each bus-load to zero
+def load_network(generation):
     net = pp.from_excel('1055-1_0_4_grid.xlsx')
+    if generation:
+        net.load.p_mw = 0.0
+        net.load.q_mvar = 0.0
+    net.sgen.p_mw = 0.0
+    net.sgen.q_mvar = 0.0
     return net
+
 
 def check_violations(net, max_voltage, min_voltage, max_line_loading, max_transformer_loading):
     try:
@@ -34,17 +40,20 @@ def check_violations(net, max_voltage, min_voltage, max_line_loading, max_transf
 pv_size = 1e-4 # Size of PV systems in MW
 ev_size = 1e-4 # Size of EVs in MW
 
-max_voltage = 5.10 # Maximum bus voltage in p.u.
+max_voltage = 1.10 # Maximum bus voltage in p.u.
 min_voltage = 0.90 # Minimum bus voltage in p.u.
-max_line_loading = 5.0 # Maximum line loading in p.u.
-max_transformer_loading = 0.2 # Maximum transformer loading in p.u.
+max_line_loading = 1.0 # Maximum line loading in p.u.
+max_transformer_loading = 1.0 # Maximum transformer loading in p.u.
 
-voltages = []
+max_voltages = []
+min_voltages = []
 loadings = []
-capacities = []
+load_capacities = []
+gen_capacities = []
 
-# Initialize the network with no PV or EV systems
-net = load_network()
+# Initialize the network with no PV or EV systems. For generation pass argument True, for no generation pass False
+generation = False
+net = load_network(generation)
 
 # Exclude the external grid from the optimization
 busses_to_test = net.bus.index[~net.bus.index.isin(net.ext_grid.bus)]
@@ -55,10 +64,11 @@ for bus in net.bus.index:
     pp.create_load(net, bus, p_mw=0.0, q_mvar=0.0)
 installed_pv = 0
 installed_ev = 0
+
 pp.runpp(net)
-pp.plotting.pf_res_plotly(net)
+pp.plotting.pf_res_plotly(net, auto_open=False)
 
-
+#%% Main Loop
 while True:
     violated, reason, index = check_violations(net, max_voltage, min_voltage, max_line_loading, max_transformer_loading)
     if violated:
@@ -66,24 +76,27 @@ while True:
         break
     # Update all buses in the test set simultaneously for a uniform growth baseline
     net.sgen.loc[net.sgen.bus.isin(busses_to_test), "p_mw"] += pv_size
-    # net.load.loc[net.load.bus.isin(busses_to_test), "p_mw"] += ev_size
+    #net.load.loc[net.load.bus.isin(busses_to_test), "p_mw"] += ev_size
 
     installed_pv += pv_size * len(busses_to_test)
-    # installed_ev += ev_size * len(busses_to_test)
+    installed_ev += ev_size * len(busses_to_test)
 
-    voltages.append(net.res_bus.vm_pu.max())
+    max_voltages.append(net.res_bus.vm_pu.max())
+    min_voltages.append(net.res_bus.vm_pu.min())
     loadings.append(net.res_line.loading_percent.max())
-    capacities.append(installed_pv)
+    load_capacities.append(installed_pv + installed_ev)
+    gen_capacities.append(installed_pv)
 
 fig, ax1 = plt.subplots()
-ax1.plot(capacities, voltages, color='b', label='Max Voltage')
+ax1.plot(load_capacities, min_voltages, color='b', label='Max Voltage')
 ax2 = ax1.twinx()
-ax2.plot(capacities, loadings, color='r', label='Max Loading')
+ax2.plot(load_capacities, loadings, color='r', label='Max Loading')
 plt.legend()
-plt.title("Grid Stress Evolution")
+plt.title("Grid Load-Stress Evolution")
 plt.show()
 
 print("Installed PV capacity: ", installed_pv, " MW")
 print("Installed EV capacity: ", installed_ev, " MW")
-pp.plotting.pf_res_plotly(net)
+print("Trafo loading: ", net.res_trafo.loading_percent.max(), "%")
+pp.plotting.pf_res_plotly(net, auto_open=False)
 #pp.plotting.simple_plot(net, respect_switches=True, plot_sgens=True, plot_loads=True)
